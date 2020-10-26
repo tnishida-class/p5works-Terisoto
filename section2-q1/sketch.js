4d787a946e14d2bfdcf21afe4c8d5289be98b89f
Terisoto <72423625+Terisoto@users.noreply.github.com>
2020-10-26 14:54:30 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,14 +1,17 @@
 // 小手調べ
 function setup(){
-  createCanvas(100,100);
+  createCanvas(1000,1000);
   noFill();
-  ellipse(50,50,i)
-  for(let i = 0; i < 10; i++){
-if(i < 5){
-  stroke(0, 0, 255);
+  for(let i = 10; i > 0; i--){
+    let x = -5 + i * 5;
+    ellipse(100,100,x);
+if(i > 4){
+  stroke(255, 0, 0);
+  strokeWeight(2)
 }
 else {
-   stroke(255, 0, 0);
+  stroke(0, 0, 255);
+  strokeWeight(2)
 }// BLANK[1]
-  }
+}
 }
