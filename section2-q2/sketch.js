292611fc0274148291f03a395e398a3f9cea66d4
Terisoto <72423625+Terisoto@users.noreply.github.com>
2020-10-27 11:05:35 +0900

    section2-q1 update

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -3,6 +3,7 @@ function setup() {
   createCanvas(200, 200);
   let size = width / 8;
   noStroke();
+  fill(128);
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       rect(size * i, size * j, size, size);// BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
