1faf1d39526f86034ab03a774d7847b06891fc70
Terisoto <72423625+Terisoto@users.noreply.github.com>
2020-10-27 10:02:53 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -3,15 +3,15 @@ function setup(){
   createCanvas(1000,1000);
   noFill();
   for(let i = 10; i > 0; i--){
-    let x = -5 + i * 5;
+    let x = 5 + i * 5;
     ellipse(100,100,x);
-if(i > 4){
+if(i > 5){
   stroke(255, 0, 0);
-  strokeWeight(2)
+  strokeWeight(1)
 }
 else {
   stroke(0, 0, 255);
-  strokeWeight(2)
+  strokeWeight(1)
 }// BLANK[1]
 }
 }
